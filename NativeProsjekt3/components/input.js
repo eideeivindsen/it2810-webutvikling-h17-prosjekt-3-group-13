e019204259143065793fa9c04e83da645a8ca0ff
junglebooboo <aigarst@stud.ntnu.no>
2017-10-02 15:41:48 +0200

    Added some components to ios native

---
+++ NativeProsjekt3/components/input.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
+
+export default class PizzaTranslator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+    
+    render() {
+        return (
+                <View style={{padding: 10}}>
+                    <Text>
+                        What is your name?
+                    </Text>
+                    <TextInput
+                    style={{height: 40}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                    />
+                    <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                    </Text>
+                </View>
+                );
+    }
+}
+
+AppRegistry.registerComponent('PizzaTranslator', () => PizzaTranslator);
