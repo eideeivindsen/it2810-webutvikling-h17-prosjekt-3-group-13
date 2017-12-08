96d96214d123e032ec4dfa5622c0446a83d3a0b8
Joachim Eivindsen <joachim.eivindsen@gmail.com>
2017-12-08 22:53:42 +0100

    Removed native app

--- Native/src/components/todo/TodoModel.js
+++
@@ -1,9 +0,0 @@
-class TodoModel {
-    constructor(title, completed) {
-        this.title = title;
-        this.completed = completed || false;
-        //this.createdAt = new Date();
-    }
-}
-
-module.exports = TodoModel;
